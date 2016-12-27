a8367d584c4b35c3d969bf7aa287fff668cf3fdf
Mixer <dantheman41@live.com.au>
2016-12-28 09:48:46 +1100

    Profiler setup

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -3,8 +3,13 @@ package;
 import kha.System;
 
 class Main {
+	static var hxt = new hxtelemetry.HxTelemetry();
+	
 	public static function main() {
 		System.init({title: "Dungeon Game", width: 1024, height: 768, samplesPerPixel: 4}, function () {
+			
+			kha.Scheduler.addFrameTask(hxt.advance_frame.bind(null), 0);
+
 			kha.Assets.loadEverything(function(){
 				new Project();
 			});
